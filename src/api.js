5bd090de8b748047f4752861526fb505cce37aef
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-03-18 22:44:42 +0100

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -53,7 +53,7 @@ module.exports = (db) => {
       
     } catch (error) {
       console.error(error);
-      res.json({note: null, error: 'Could not find note, perhaps it has already been destroyed?'});
+      res.json({note: null, error: 'Could not find note.'});
     }
     
   });
@@ -75,7 +75,7 @@ module.exports = (db) => {
     try {
       const encryptedNote = await db.collection('notes').findOne({securityKey});
       if (!encryptedNote) {
-        res.render('read', {note: null, error: 'Could not find note, perhaps it has already been destroyed?'});
+        res.render('read', {note: null, error: 'Could not find note.'});
       } else {
         if (encryptedNote.ttl == 0) { // non-timed ttl
           try {
@@ -116,7 +116,7 @@ module.exports = (db) => {
     try {
       const encryptedNote = await db.collection('notes').findOne({securityKey});
       if (!encryptedNote) {
-        res.render('read', {note: null, error: 'Could not find note, perhaps it has already been destroyed?'});
+        res.render('read', {note: null, error: 'Could not find note.'});
       } else {
         try {
           const ascii = crypto.isASCII(securityKey + iv);
